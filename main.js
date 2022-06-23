7d1280a50e5fe91d0e8d21055f88aaf7d6675a02
Florence113 <florencejacintaa02@gmail.com>
2022-06-24 00:41:42 +0100

    First commit of simpleoutput

---
+++ main.js
@@ -0,0 +1,12 @@
+const myName = 'Asoegwu Florence Nneoma';
+const myHeight = '5 ft 5 in';
+const myCountry = 'Nigeria';
+
+const result = (`Name: ${myName} \nHeight: ${myHeight} \nCountry: ${myCountry}`);
+const sentence = myName.concat(' is my name, I am ', myHeight, ' tall and I am from ', myCountry, '. Thank You.');
+
+alert(myName);
+alert(myHeight);
+alert(myCountry);
+alert(result);
+alert(sentence);
